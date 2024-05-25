d3f949bfb1cb048c65f7bafc47f0c4f1fec331a2
Abdul-Wahib <wahibabdul.aw@gmail.com>
2024-05-25 16:17:17 +0200

    Project Created!

---
+++ Fotos/FotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  FotosApp.swift
+//  Fotos
+//
+//  Created by Abdul Wahib on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct FotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
